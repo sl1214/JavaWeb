bc7fded861ee2873c5e5c9cbefb8258a240e538b
sl1214 <894654642@qq.com>
2020-08-27 17:04:09 +0800

    删除多余目录

--- JavaWeb/src/server/Servlet.java
+++
@@ -1,8 +0,0 @@
-package server;
-
-import java.io.OutputStream;
-
-public interface Servlet {
-    public void inint() throws Exception;
-    public void service(byte[] requestBuffer, OutputStream out) throws Exception;
-}
